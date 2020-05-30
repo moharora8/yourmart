2ba7a882f180175696f7c15a87bb81052ec00062
moharora8 <moharora8@gmail.com>
2020-05-31 01:27:05 +0530

    first commit

---
+++ djecommerce/settings/production.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
+    }
+}
+
+STRIPE_PUBLISHABLE_KEY = config('pk_test_IyLbILlBLPao604HsmkVhXmp00NPyafhKP')
+STRIPE_SECRET_KEY = config('sk_test_t2W75KZS9YOqbTR1WLmi6AMZ00Ukg063SQ')
